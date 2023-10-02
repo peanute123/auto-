25c08c731ae099c6a98ca4b299ce05b51f4aa156
peanute123 <69617935+peanute123@users.noreply.github.com>
2023-10-02 22:55:05 +0800

    Update run.sh

--- 服务端/run.sh
+++ 服务端/run.sh
@@ -1,4 +1,6 @@
-git clone https://github.com/JaidedAI/EasyOCR.git
+# git clone https://github.com/JaidedAI/EasyOCR.git
+pip install EasyOCR
+pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
 pip install cnsenti -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install -U Flask
 python one_flask.py
