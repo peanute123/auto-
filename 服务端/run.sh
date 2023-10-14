8d2a587f16d51c43e6b7d23475efc42a38840494
peanute123 <69617935+peanute123@users.noreply.github.com>
2023-10-14 21:29:27 +0800

    Update run.sh

--- 服务端/run.sh
+++ 服务端/run.sh
@@ -3,7 +3,7 @@
 # pip install Pillow
 # pip install torch 
 pip --no-cache-dir  install  EasyOCR
-pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+//pip install opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple 这个安装太坏了，不如EasyOCR装的
 pip install cnsenti -i https://pypi.tuna.tsinghua.edu.cn/simple/
 pip install -U Flask
 python one_flask.py
